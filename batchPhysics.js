f333481b642b9eb0c1a76f10672e83b416b0d5dd
Ben Brown <ben.raymond.brown@gmail.com>
2015-12-14 23:51:22 -0500

    Break up relative position calculation
    Subtracting x_i from x_j for every {i,j}==> r_ij is expensive.
    Breaking this up into smaller, digestible peices:
    
    1. Store a copy of x_i in each row of r_ij.
    2. Store a copy of x_j in each column of r2_ij.
    3. r_ij -= r2_ij.
    
    Amazingly, step (2) is easy to make extremely cheap, step(1) is
    somewhat cheap, and step 3 is expensive.  Combining (1) and (3) yields
    no savings.

--- batchPhysics.js
+++ batchPhysics.js
@@ -40,14 +40,19 @@ And Particle will therefore see this transparently.
         this.forceTable = allocateTViewSpace(this.workBuffer,
                 3 * this.numTableElements);
 
+        this.tmpSelfPosition = new this.tView(this.workBuffer,
+                this.workBuffer.nextByte,
+                3 * this.numTableElements);
+
+        this.distSquareTable = allocateTViewSpace(this.workBuffer,
+                1 * this.numTableElements);
+
         this.forceStrengthTable = allocateTViewSpace(this.workBuffer,
                 1 * this.numTableElements);
 
         this.distTable = allocateTViewSpace(this.workBuffer,
                 1 * this.numTableElements);
 
-        this.distSquareTable = allocateTViewSpace(this.workBuffer,
-                1 * this.numTableElements);
     };
 
     this.copy = function (includeWorkspace) {
@@ -148,62 +153,146 @@ BruteFrog.prototype.setGravityForce = function () {
         }
     };
     setx(this);
-    //Subtract self position to form vector pointing from self to every other:
 
-
-    var setdxOptimized = function (f) {
-        var j = -1;
-        var x, y, z;
-        var numElements;
+    var setSelfx = function (f) {
+        var numElements, numVColumns;
+        var selfI;
         numElements = 3 * f.numTableElements;
+        numVColumns = 3 * f.numColumns;
+
         for (i = 0; i < numElements; i += 3) {
-            if (!(0)) {
-                j += 1;
-                x = f.x[j + 0];
-                y = f.x[j + 1];
-                z = f.x[j + 2];
-            }
-            f.forceTable[i + 0] -= x;
-            f.forceTable[i + 1] -= y;
-            f.forceTable[i + 2] -= z;
+            selfI = (i / numVColumns) >>> 0;
+            f.tmpSelfPosition[i + 0] = f.x[selfI + 0];
+            f.tmpSelfPosition[i + 1] = f.x[selfI + 1];
+            f.tmpSelfPosition[i + 2] = f.x[selfI + 2];
         }
     };
-    setdxOptimized(this);
+    setSelfx(this);
 
-    // var setdx = function (f) {
-    //     for (iVector = 0; iVector < 3 * f.numTableElements; iVector += 3 * f.numColumns) {
-    //         for (jVector = 0; jVector < 3 * f.numColumns; jVector += 3) {
-    //             f.forceTable[iVector + jVector + 0] -= f.x[iVector + 0];
-    //             f.forceTable[iVector + jVector + 1] -= f.x[iVector + 1];
-    //             f.forceTable[iVector + jVector + 2] -= f.x[iVector + 2];
-    //         }
-    //     }
-    // };
-    // setdx(this);
+    var setdxSubtract = function (f) {
+        var BLOCK_SIZE = 8;
+        var numElements = 3 * f.numTableElements;
+
+        var iChunked = BLOCK_SIZE * Math.floor(numElements / BLOCK_SIZE);
+
+        for (i = 0; i < iChunked; i += BLOCK_SIZE) {
+            f.forceTable[i + 0] -= f.tmpSelfPosition[i + 0];
+            f.forceTable[i + 1] -= f.tmpSelfPosition[i + 1];
+            f.forceTable[i + 2] -= f.tmpSelfPosition[i + 2];
+            f.forceTable[i + 3] -= f.tmpSelfPosition[i + 3];
+            f.forceTable[i + 4] -= f.tmpSelfPosition[i + 4];
+            f.forceTable[i + 5] -= f.tmpSelfPosition[i + 5];
+            f.forceTable[i + 6] -= f.tmpSelfPosition[i + 6];
+            f.forceTable[i + 7] -= f.tmpSelfPosition[i + 7];
+        }
+
+        for (i = iChunked; i < numElements; i += 1) {
+            f.forceTable[i + 0] -= f.tmpSelfPosition[i + 0];
+        }
+    };
+    setdxSubtract(this);
+
+
+    var setGravityToAwesome = function (f) {
+        var rSq;
+        var numElements = 3 * f.numTableElements;
+
+        for (i = 0; i < numElements; i += 3) {
+            rSq = 1 / (f.forceTable[i + 0] * f.forceTable[i + 0] +
+                    f.forceTable[i + 1] * f.forceTable[i + 1] +
+                    f.forceTable[i + 2] * f.forceTable[i + 2]);
+            rSq *= Math.sqrt(rSq);
+            f.forceTable[i + 0] *= rSq;
+            f.forceTable[i + 1] *= rSq;
+            f.forceTable[i + 2] *= rSq;
+        }
+    };
+    setGravityToAwesome(this);
+
+
+
+
+
+
 
 
     //Calculate d^2
-    var setdSquaredOptimized = function (f) {
-        for (i = 0; i < f.numTableElements; i += 1) {
-            f.distSquareTable[i] = f.forceTable[3 * i + 0] * f.forceTable[3 * i + 0];
-            f.distSquareTable[i] += f.forceTable[3 * i + 1] * f.forceTable[3 * i + 1];
-            f.distSquareTable[i] += f.forceTable[3 * i + 2] * f.forceTable[3 * i + 2];
+    var setdSquared = function (f) {
+        var numElements;
+        var iChunked;
+        var BLOCK_SIZE = 2;
+        numElements = f.numTableElements;
+        iChunked = BLOCK_SIZE * Math.floor(numElements / BLOCK_SIZE);
+
+
+        // for (i = 0; i < iChunked; i += BLOCK_SIZE) {
+        //     f.distSquareTable[i] = f.forceTable[3 * i + 0] * f.forceTable[3 * i + 0];
+        //     f.distSquareTable[i] += f.forceTable[3 * i + 1] * f.forceTable[3 * i + 1];
+        //     f.distSquareTable[i] += f.forceTable[3 * i + 2] * f.forceTable[3 * i + 2];
+        //     f.distSquareTable[i + 1] = f.forceTable[3 * i + 3] * f.forceTable[3 * i + 3];
+        //     f.distSquareTable[i + 1] += f.forceTable[3 * i + 4] * f.forceTable[3 * i + 4];
+        //     f.distSquareTable[i + 1] += f.forceTable[3 * i + 5] * f.forceTable[3 * i + 5];
+        // }
+        iChunked = 0;
+        var iV = 0;
+        for (i = iChunked; i < numElements; i += 1) {
+            f.distSquareTable[i] = f.forceTable[iV + 0] * f.forceTable[iV + 0];
+            f.distSquareTable[i] += f.forceTable[iV + 1] * f.forceTable[iV + 1];
+            f.distSquareTable[i] += f.forceTable[iV + 2] * f.forceTable[iV + 2];
+            iV += 3;
+        }
+
+    };
+    // setdSquared(this);
+
+
+    var calcSquaredOptimized = function (f) {
+        var BLOCK_SIZE = 8;
+        var numElements = 3 * f.numTableElements;
+
+        f.tmpSelfPosition.set(f.forceTable);
+
+        var iChunked = BLOCK_SIZE * Math.floor(numElements / BLOCK_SIZE);
+        for (i = 0; i < iChunked; i += BLOCK_SIZE) {
+            f.tmpSelfPosition[i + 0] *= f.tmpSelfPosition[i + 0];
+            f.tmpSelfPosition[i + 1] *= f.tmpSelfPosition[i + 1];
+            f.tmpSelfPosition[i + 2] *= f.tmpSelfPosition[i + 2];
+            f.tmpSelfPosition[i + 3] *= f.tmpSelfPosition[i + 3];
+            f.tmpSelfPosition[i + 4] *= f.tmpSelfPosition[i + 4];
+            f.tmpSelfPosition[i + 5] *= f.tmpSelfPosition[i + 5];
+            f.tmpSelfPosition[i + 6] *= f.tmpSelfPosition[i + 6];
+            f.tmpSelfPosition[i + 7] *= f.tmpSelfPosition[i + 7];
+        }
+
+        for (i = iChunked; i < numElements; i += 1) {
+            f.tmpSelfPosition[i + 0] *= f.tmpSelfPosition[i + 0];
+        }
+
+    };
+    // calcSquaredOptimized(this);
+
+    var sumSquaredOptimized = function (f) {
+        var iV = 0;
+        var numElements = f.numTableElements;
+        for (i = 0; i < numElements; i += 1) {
+            f.distSquareTable[i + 0] = f.tmpSelfPosition[iV + 0];
+            f.distSquareTable[i + 0] += f.tmpSelfPosition[iV + 1];
+            f.distSquareTable[i + 0] += f.tmpSelfPosition[iV + 2];
+            iV += 3;
         }
     };
+    // sumSquaredOptimized(this);
 
-    setdSquaredOptimized(this);
-
-
-    BruteFrog.prototype.sqrts(this.distSquareTable, this.distTable);
+    // BruteFrog.prototype.sqrts(this.distSquareTable, this.distTable);
 
 
 
-    var calculateInverseCube = function (f) {
+    var scaleInverseCube = function (f) {
         for (i = 0; i < f.numTableElements; i += 1) {
             f.forceStrengthTable[i] = 1 / (f.distSquareTable[i] * f.distTable[i]);
         }
     };
-    calculateInverseCube(this);
+    // scaleInverseCube(this);
 
     var scaleM = function (f) {
         for (jScalar = 0; jScalar < f.numColumns; jScalar += 1) {
@@ -219,14 +308,14 @@ BruteFrog.prototype.setGravityForce = function () {
         iScalar += 1; //To follow the diagonal elements.
     }
 
-    var scaleByInverseCubeOptimized = function (f) {
-        for (i = 0; i < f.numTableElements; i += 1) {
-            f.forceTable[3 * i + 0] *= f.distSquareTable[i];
-            f.forceTable[3 * i + 1] *= f.distSquareTable[i];
-            f.forceTable[3 * i + 2] *= f.distSquareTable[i];
-        }
-    };
-    scaleByInverseCubeOptimized(this);
+    // var scaleByInverseCubeOptimized = function (f) {
+    //     for (i = 0; i < f.numTableElements; i += 1) {
+    //         f.forceTable[3 * i + 0] *= f.distSquareTable[i];
+    //         f.forceTable[3 * i + 1] *= f.distSquareTable[i];
+    //         f.forceTable[3 * i + 2] *= f.distSquareTable[i];
+    //     }
+    // };
+    // scaleByInverseCubeOptimized(this);
 
 
     this.a.fill(0);
